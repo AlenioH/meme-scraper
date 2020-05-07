383a1c10030d4066bb598acea5068a749b2fbe14
Alena Hasslacher <alena.hasslacher@gmail.com>
2020-05-07 16:38:55 +0200

    Fix apostrophe issue, move 1 require to the beginning

--- index.js
+++ index.js
@@ -1,20 +1,19 @@
 const request = require('request');
 const imageDownloader = require('node-image-downloader');
+const cheerio = require('cheerio');
 
 const memeUrl = request('https://memegen.link/examples', function (
   err,
   res,
   body,
 ) {
-  const cheerio = require('cheerio');
   const $ = cheerio.load(body);
   const elements = $('.row a');
 
   for (let i = 0; i < 10; i++) {
     const url = 'https://memegen.link/';
 
-    const links =
-      url + elements[i].attribs.href.split('?')[0].replace(/[']/g, '');
+    const links = url + elements[i].attribs.href.split('?')[0];
 
     imageDownloader({
       imgs: [
