ee44213c3e2104f274eb9f87eb89f1d1a04326e5
Alena Hasslacher <alena.hasslacher@gmail.com>
2020-05-08 21:36:27 +0200

    Add comments + fs create folder + filenames

--- index.js
+++ index.js
@@ -1,25 +1,27 @@
 const request = require('request');
 const imageDownloader = require('node-image-downloader');
 const cheerio = require('cheerio');
+const fs = require('fs');
 
-const memeUrl = request('https://memegen.link/examples', function (
-  err,
-  res,
-  body,
-) {
-  const $ = cheerio.load(body);
-  const elements = $('.row a');
+//creates a folder
+fs.mkdirSync('memes');
 
+//connects to the web-site
+request('https://memegen.link/examples', function (err, res, body) {
+  const $ = cheerio.load(body); //gets the html code of the entire site body
+  const elements = $('.row a'); //gets the html codes of the elemets .row a
+
+  //the loop sorts out the first 10 images and downloads them
   for (let i = 0; i < 10; i++) {
     const url = 'https://memegen.link/';
 
-    const links = url + elements[i].attribs.href.split('?')[0];
+    const links = url + elements[i].attribs.href.split('?')[0]; //the path to the individual links of the images
 
     imageDownloader({
       imgs: [
         {
           uri: links,
-          filename: '0' + i,
+          filename: `${i}`,
         },
       ],
       dest: './memes',
