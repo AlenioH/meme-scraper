6b67d4f59ca8b3b15531adb05933acb33d609c70
Alena Hasslacher <alena.hasslacher@gmail.com>
2020-05-24 13:35:10 +0200

    Add function that checks if the directory already exists + log out download info

--- index.js
+++ index.js
@@ -3,8 +3,12 @@ const imageDownloader = require('node-image-downloader');
 const cheerio = require('cheerio');
 const fs = require('fs');
 
-//creates a folder
-fs.mkdirSync('memes');
+//the function checks if the fir alrrady exists, if it doen't the function creates one
+fs.exists('./memes', function (exists) {
+  if (!exists) {
+    fs.mkdirSync('./memes');
+  }
+});
 
 //connects to the web-site
 request('https://memegen.link/examples', function (err, res, body) {
@@ -15,16 +19,23 @@ request('https://memegen.link/examples', function (err, res, body) {
   for (let i = 0; i < 10; i++) {
     const url = 'https://memegen.link/';
 
-    const links = url + elements[i].attribs.href.split('?')[0]; //the path to the individual links of the images
+    const linkString = url + elements[i].attribs.href.split('?')[0]; //the path to the individual links of the images
 
     imageDownloader({
       imgs: [
         {
-          uri: links,
+          uri: linkString,
           filename: `${i}`,
         },
       ],
       dest: './memes',
-    });
+    })
+      .then((info) => {
+        console.log('Image downloaded', info);
+      })
+      .catch((error, response, body) => {
+        console.log('Something went wrong!');
+        console.log(error);
+      });
   }
 });
